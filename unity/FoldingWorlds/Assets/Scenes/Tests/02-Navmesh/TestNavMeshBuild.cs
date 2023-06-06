6e8c54f25b6ceb325e0ade35fa01f4e7e6e86e62
Tibor Udvari <tibor.udvari@icloud.com>
2023-06-06 11:10:53 +0200

    Import dynamic navmesh building components

---
+++ unity/FoldingWorlds/Assets/Scenes/Tests/02-Navmesh/TestNavMeshBuild.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestNavMeshBuild : MonoBehaviour
+{
+    void Start()
+    {
+        
+        Debug.Log("hellooo");
+    }
+
+    void Update()
+    {
+        
+    }
+}
