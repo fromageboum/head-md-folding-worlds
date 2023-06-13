696f9b3c24f57ee984f0a69bd524023e85483928
Tibor Udvari <tibor.udvari@icloud.com>
2023-06-13 09:11:11 +0200

    Website video rendering update

--- website/scripts/concat.js
+++ website/scripts/concat.js
@@ -14,7 +14,6 @@ const inputFiles = fs.readdirSync(directory)
 
 inputFiles.forEach(inputFile => {
     let data = fs.readFileSync(path.join(directory, inputFile), 'utf8');
-    output += `\n<!-- File: ${inputFile} -->\n`;
     output += data;
 });
 
