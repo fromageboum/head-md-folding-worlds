e69194604f0a30f1036cc2072449f26d34500a1b
Raphaël Munoz <raphael@aprobado.ch>
2023-10-22 13:24:01 +0200

    trying to mix openXR interaction with Oculus Passthrough camera

--- unity/FoldingWorlds/Assets/Scripts/Interaction/CalibrateTablePosition.cs
+++ unity/FoldingWorlds/Assets/Scripts/Interaction/CalibrateTablePosition.cs
@@ -31,11 +31,27 @@ public class CalibrateTablePosition : MonoBehaviour
 
     private void CalibrateAction_Onperformed(InputAction.CallbackContext ctx)
     {
+        if (!ControllerPositionIsValid()) return;
+        
         var originalParent = TabletopRootTransform.parent;
+        
         // TODO: Get the angle on XZ plane then use an upward Y axis to be sure we're flat.
-        TabletopRootTransform.SetParent(PhysicalReferenceTransform);
-        TabletopRootTransform.localPosition = Vector3.zero;
-        TabletopRootTransform.localRotation = Quaternion.identity;
-        TabletopRootTransform.SetParent(originalParent, true);
+
+        var physicalForward = PhysicalReferenceTransform.forward;
+        physicalForward.y = 0f;
+        physicalForward.Normalize();
+        var calibrationRotation = Quaternion.LookRotation(physicalForward, Vector3.up);
+        TabletopRootTransform.rotation = calibrationRotation;
+        
+        // TabletopRootTransform.SetParent(PhysicalReferenceTransform);
+        // TabletopRootTransform.localPosition = Vector3.zero;
+        // TabletopRootTransform.localRotation = Quaternion.identity;
+        // TabletopRootTransform.SetParent(originalParent, true);
+    }
+
+    private bool ControllerPositionIsValid()
+    {
+        // a dot product of 0.95 is ~= 18°
+        return Vector3.Dot(Vector3.up, PhysicalReferenceTransform.up) > 0.95f;
     }
 }
